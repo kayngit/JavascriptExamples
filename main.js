c725e9cb85ee80782381644f63ce81bf01392124
kay-n <kay-n@ya.ru>
2021-09-03 12:42:45 +0300

    added many balls

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 function animation() {
-    x += vectorX;
-    y += vectorY;
+    for (let i = 0; i < balls.length; i++) {
+        const ballObject = balls[i];
+        ballObject.ball.style.left = ballObject.x + 'px';
+        ballObject.ball.style.top = ballObject.y + 'px';
 
-    if (x > 380) {
-        vectorX = -1;
-    } else if (x < 0) {
-        vectorX = 1;
+        ballObject.x += ballObject.vectorX;
+        ballObject.y += ballObject.vectorY;
+
+        if (ballObject.x > 380) {
+            ballObject.vectorX = -1 - Math.random();
+        } else if (ballObject.x < 0) {
+            ballObject.vectorX = 1 + Math.random();
+        }
+
+        if (ballObject.y > 380) {
+            ballObject.vectorY = -1 - Math.random();
+        } else if (ballObject.y < 0) {
+            ballObject.vectorY = 1 + Math.random();
+        }
     }
-
-    if (y > 380) {
-        vectorY = -2;
-    } else if (y < 0) {
-        vectorY = 2;
-    }
-
-    ball.style.left = x + 'px';
-    ball.style.top = y + 'px'
 }
 
+const cont = document.querySelector('#cont');
 
-const ball = document.querySelector('#ball')
-
-let x = 28;
-let y = 137;
 let vectorX = 1;
 let vectorY = 2;
 
+const balls = [];
+for (let i = 0; i < 100; i++) {
+    const ball = document.createElement('div');
+    console.log(ball)
+    ball.setAttribute('class', 'ball');
+    ball.style['background-color'] = `rgb(${Math.random() * 255},${Math.random() * 255},${Math.random() * 255})`;
+    const x = Math.floor(Math.random() * 380);
+    const y = Math.floor(Math.random() * 380);
+    balls.push({
+        ball,
+        x,
+        y,
+        vectorX: 1 + Math.random(),
+        vectorY: 1 + Math.random(),
+    });
+    cont.appendChild(ball);
+}
 
-ball.style.left = x + 'px';
-ball.style.top = y + 'px';
 
 setInterval(animation, 10);
